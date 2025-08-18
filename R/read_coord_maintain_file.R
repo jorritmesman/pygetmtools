bd0faecacec1b723fd0a8cf9ba214b2e0aaba508
Jorrit Mesman <jpmesman@gmail.com>
2025-08-18 15:03:12 +0200

    Make ncdf argument optional

--- R/read_coord_maintain_file.R
+++ R/read_coord_maintain_file.R
@@ -3,7 +3,8 @@
 #' @param filepath  character; path to a csv file with columns "x" and "y"
 #'   (or "ind_x" and "ind_y" already). Optional column 'fix_depth'; any values
 #'   filled here are used instead of the original bathy's depth. 
-#' @param ncdf character; path to a PyGETM bathymetry netcdf file
+#' @param ncdf character; path to a PyGETM bathymetry netcdf file. Not needed if
+#'   'ind_x' and 'ind_y' columns are already provided.
 #' 
 #' @examples
 #' \dontrun{
@@ -12,23 +13,25 @@
 #' @import ncdf4
 #' @export
 
-read_coord_maintain_file = function(filepath, ncdf){
-  nc = nc_open(ncdf, write = T)
-  x_coord = ncvar_get(nc, varid = "x")
-  y_coord = ncvar_get(nc, varid = "y")
-  nc_close(nc)
+read_coord_maintain_file = function(filepath, ncdf = NULL){
+  if(!is.null(ncdf)){
+    nc = nc_open(ncdf, write = T)
+    x_coord = ncvar_get(nc, varid = "x")
+    y_coord = ncvar_get(nc, varid = "y")
+    nc_close(nc)
+  }
   
   df = fread(filepath)
   
-  # Set limits of dimensions
-  max_x = length(x_coord)
-  max_y = length(y_coord)
-  
   # Calculate indices of df and keep indices that are inside the bounds
   if(!("ind_x" %in% names(df))){
+    if(is.null(ncdf)) stop("No 'ncdf' argument provided nor a 'ind_x' and 'ind_y' column!")
+    
     df[, ind_x := sapply(x, function(i) get_ind(i, x_coord, rule = "NA"))]
   }
   if(!("ind_y" %in% names(df))){
+    if(is.null(ncdf)) stop("No 'ncdf' argument provided nor a 'ind_x' and 'ind_y' column!")
+    
     df[, ind_y := sapply(y, function(i) get_ind(i, y_coord, rule = "NA"))]
   }
   df = df[!is.na(ind_x) & !is.na(ind_y)]
