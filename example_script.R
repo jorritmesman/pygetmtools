0a96767d7f9aefaa0263cabd09e68671892d3d9a
Jorrit Mesman <jpmesman@gmail.com>
2025-11-06 14:44:50 +0100

    Add extra features to example script

--- example_script.R
+++ example_script.R
@@ -113,3 +113,87 @@ ggsave("example_plot_pygetmtools.png", width = 8, height = 6)
 # By default, this saves the last plot you made. You can make this more explicit
 # assigning the plot to a variable (e.g. p = ggplot(...) etc.) and then specifying
 # ggsave("path/to/file.png", plot = p)
+
+##### More specific and advanced usage #####
+# In the next part of this script, you can find some more advanced features and
+# uses of the package. 
+
+### Inflow file generation -----
+# This creates a netcdf inflow file from a csv. The time series in the csv does
+# not need to be continuous.
+
+# Create an example csv file, with two short pulses of a tracer called "Tracer"
+df_tracer = data.table(date = as.POSIXct(c("2014-01-01", "2014-04-01", "2014-04-02",
+                                           "2014-04-03", "2014-04-04", "2014-07-01",
+                                           "2014-07-02", "2014-07-03", "2014-07-04",
+                                           "2015-01-01"), tz = "UTC"),
+                       flow = c(0,0,1.0,1.0,0,0,1,1,0,0),
+                       Tracer_c = c(0,0,100,100,0,0,100,100,0,0))
+ggplot(df_tracer) +
+  geom_line(aes(date, Tracer_c)) +
+  geom_point(aes(date, Tracer_c))
+fwrite(df_tracer, "tracer_file.csv")
+
+# Use function to generate a netcdf file that can be read by pygetm's read_from_netcdf function
+create_inflow_nc_from_csv("tracer_file.csv", lat = 59.44, lon = 17.43,
+                          file_out = "example_tracer_file.nc", timestep = "1 hour",
+                          fabmyaml = NULL)
+
+### Bathymetry smoothing -----
+# Step 1: read bathymetry as a matrix into R
+bathy = read_bathy_as_matrix(bathy_nc, var_name = depth_name)
+plot_bathy(bathy) # Add "rev_x = T" and/or "rev_y = T" if your coordinates are provided
+                  # in descending order
+
+# Step 2: Smooth and plot repeatedly until you are happy with the result.
+# Note that the smooth_bathy_matrix functions has a LOT of options, and the most
+# important one is to fix certain coordinates and expand from those ("local" smoothing)
+# or just use a moving window (then set maintain_coords to NULL), or a combination.
+# By default, you set a maximum value for "rx0". rx0 =  delta-H / (2 * mean(H1,H2)),
+# Beckmann & Haidvogel, 1993, doi:10.1175/1520-0485(1993)023<1736:NSOFAA>2.0.CO;2
+bathy_s1 = smooth_bathy_matrix(bathy, max_val = 0.15, method = "rx0",
+                               maintain_coords = NULL,
+                               adjust_factor_global = 0.02)
+plot_bathy(bathy_s1, mtrx_ref = bathy, rev_y = T)
+plot_bathy(bathy_s1, plot_slope_method = "rx0", rev_y = T)
+
+# Step 3: add the smoothed bathymetry to the netcdf
+add_bathy_to_ncdf(bathy_s1, ncdf = bathy_nc, depth_name_new = "bathy_smoothed2",
+                  depth_name_orig = depth_name)
+
+### Calculate flow through an interface -----
+# Specify the x and y and direction of the flow (x/y). "sign" and "interface_pos" are optional and
+# "1" by default
+the_interface = list(list(x = 594750, y = 6599850, direction = "y", sign = -1, interface_pos = 1),
+                     list(x = 594750, y = 6599850, direction = "x", sign = 1, interface_pos = 1),
+                     list(x = 594450, y = 6599850, direction = "y", sign = -1, interface_pos = 1),
+                     list(x = 594150, y = 6599250, direction = "x", sign = 1, interface_pos = 1))
+
+# Plot to see if the interface is what you intend it to be ("plotting_window", "arrow_length", and "arrow_tip"
+# can be used to modify the plot)
+plot_interface(ncdfs = output_ncs, interface = the_interface,
+               plotting_window = 5L)
+
+# Calculate the flow through the interface (in m3/s)
+df_flow = flux_interface(ncdfs = output_ncs, interface = the_interface)
+ggplot(df_flow) +
+  geom_line(aes(date, flow))
+
+### Pair observations and simulations -----
+# This function finds the closest modelled value to your observations (in terms
+# of x, y, z, and time). It does not interpolate.
+
+# Create example csv
+df_obs = data.table(date = as.POSIXct(c("2015-04-08 12:00:00", "2015-04-08 12:00:00")),
+                    x = c(646548, 646548), y = c(6626576, 6626576), depth = c(0.0, -10.0),
+                    temp = c(5.1, 4.8))
+fwrite(df_obs, "obs_file.csv")
+
+# Result is returned as a list, with a data.table for each variable requested.
+# The list_vars argument is in the format "pygetm_name = obs_file_column_name"
+# If the closest simulation is more than max_time_diff away, it will not be used
+lst_paired = pair_sim_and_obs(ncs = output_ncs,
+                              obs = "obs_file.csv",
+                              list_vars = list(temp = "temp"),
+                              max_time_diff = lubridate::days(3))
+df_temp = lst_paired[["temp"]]
