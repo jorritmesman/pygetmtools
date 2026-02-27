e80c5f5ef4aac985ebde55919308ccfa2708f416
Jorrit Mesman <jpmesman@gmail.com>
2026-02-27 17:16:17 +0100

    create inflow nc now works outside UTC and added flow_col

--- R/create_inflow_nc_from_csv.R
+++ R/create_inflow_nc_from_csv.R
@@ -16,14 +16,15 @@
 #' that the unit of the input is the same as that is used by PyGETM. For tracers,
 #' the unit may be arbitrary, but PyGETM will use the same unit as is in the inflow
 #' 
-#' @param filename character; Point to csv file with headers ('date', 'flow', and any constituents).
-#' 'date' needs to have format 'yyyy-mm-dd hh:mm:ss' and 'flow' must be in m3/s
+#' @param filename character; Point to csv file with headers ('date', flow_col, and any constituents).
+#' 'date' needs to have format 'yyyy-mm-dd hh:mm:ss' and flow_col must be in m3/s
 #' @param lat,lon numeric; latitude and longitude in decimal degrees (north/east)
 #' @param file_out character; path to output netcdf file
 #' @param fabmyaml character; path to fabm.yaml file used for simulation. If provided, will check the constituents in the file
 #' @param timestep character; interpretable by seq.POSIXct
 #' @param origin_getm character; 'yyyy-mm-dd hh:mm:ss' format, time origin used in GETM nc file
 #' @param plot logical; if true, makes a plot of the output that was written
+#' @param flow_col character; the name of the column with discharge values. default = "flow"
 #' @author
 #' Jorrit Mesman
 #' @examples
@@ -46,14 +47,14 @@
 create_inflow_nc_from_csv = function(filename, lat, lon, file_out,
                                      fabmyaml = NULL, timestep = "1 hour",
                                      origin_getm = "1900-01-01 00:00:00",
-                                     plot = FALSE){
+                                     plot = FALSE, flow_col = "flow"){
   # Read file
   df = fread(filename)
   
   # Check validity of filename
-  if(!("date" %in% names(df) & "flow" %in% names(df))){
+  if(!("date" %in% names(df) & flow_col %in% names(df))){
     stop("'filename' needs to have, at least, a column named 'date' and a column ",
-         "named 'flow'!")
+         "named ", flow_col, "!")
   }
   
   if(any(duplicated(df$date))){
@@ -64,7 +65,7 @@ create_inflow_nc_from_csv = function(filename, lat, lon, file_out,
   
   # If other columns are present; check. Also uses the fabmyaml argument, if given.
   if(length(names(df) > 2)){
-    col_names = names(df)[!(names(df) %in% c("date", "flow", "temp", "salt"))]
+    col_names = names(df)[!(names(df) %in% c("date", flow_col, "temp", "salt"))]
     for(i in col_names){
       spl = strsplit(i, "_")[[1]]
       if(length(spl) == 1L){
@@ -102,7 +103,7 @@ create_inflow_nc_from_csv = function(filename, lat, lon, file_out,
   }
   
   # Create netcdf
-  times_nc = as.numeric(difftime(df_write$all_times, as.POSIXct(origin_getm), units = "hours"))
+  times_nc = as.numeric(difftime(df_write$all_times, as.POSIXct(origin_getm, tz = "UTC"), units = "hours"))
   timedim = ncdim_def("time", units = paste0("hours since ", origin_getm), vals = times_nc, unlim = T)
   lon_dim = ncdim_def("lon", "degrees_east", vals = ifelse(lon >= 0, lon, lon + 360), longname = "lon")
   lat_dim = ncdim_def("lat", "degrees_north", vals = lat, longname = "lat")
@@ -110,7 +111,7 @@ create_inflow_nc_from_csv = function(filename, lat, lon, file_out,
   vars = names(df_write)[-1]
   var_list = vector("list", length = length(vars))
   for(i in seq_along(vars)){
-    the_unit = fifelse(vars[i] == "flow", "m3/s", "-")
+    the_unit = fifelse(vars[i] == flow_col, "m3/s", "-")
     nc_var = ncvar_def(vars[i], the_unit, list(lon_dim, lat_dim, timedim),
                        longname = vars[i], prec = "float", compression = 5L, shuffle = FALSE)
     var_list[[i]] = nc_var
